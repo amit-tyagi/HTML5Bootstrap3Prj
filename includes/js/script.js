4bcd5454edadf822fcb3a017081fae2a0062e048
Amit Tyagi <iamittyagi@gmail.com>
2014-08-16 15:35:00 -0500

    Added tab functionality.

--- includes/js/script.js
+++ includes/js/script.js
@@ -3,8 +3,25 @@
 My Custom JS
 ============
 
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
+Author:  Amit Tyagi
+Updated: August 2014
 
 */
+
+$(function() {
+
+    $('#alertMe').click(function(e) {
+        e.preventDefault();
+
+        $('#successAlert').slideDown();
+    });
+
+    $('a.pop').click(function(e) {
+        e.preventDefault();
+    });
+
+    $('a.pop').popover();
+
+    $('[rel="tooltip"]').tooltip();
+
+});
